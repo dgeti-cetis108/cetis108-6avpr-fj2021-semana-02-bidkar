8266eb586bf3852209855e1a9cb8a47a7670e27d
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-02-24 14:03:32 -0700

    Primera versión
    Practicas de la semana 02.

---
+++ app/src/main/java/com/cetis108/myfirstandroidapplication6avpr/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cetis108.myfirstandroidapplication6avpr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
