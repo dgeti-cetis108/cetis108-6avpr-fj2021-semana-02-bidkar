9c7c5da198d8fc4cc655de7582862e06abf3739d
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-02-26 14:32:55 -0700

    Fundamentos de la UI
    UI Design.

---
+++ app/src/main/java/com/cetis108/myfirstandroidapplication6avpr/HelloActivity.kt
@@ -0,0 +1,13 @@
+package com.cetis108.myfirstandroidapplication6avpr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+
+        // R.id.btHelloSaludar
+    }
+}
